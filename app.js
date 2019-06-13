31fe7ce51ada57c8f3553018ba12df312a595f73
Federico Gervasoni <federicogervasonivl@gmail.com>
2019-06-13 14:41:29 +0000

    fix mongooose connect real

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     
 
 // seedDB();
-mongoose.connect("mongoose.connect(mongodb+srv://superuser:Hogwarts1990@cluster0-ghngh.mongodb.net/yelp_camp?retryWrites=true&w=majority)", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://superuser:Hogwarts1990@cluster0-ghngh.mongodb.net/yelp_camp?retryWrites=true&w=majority");
 mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
